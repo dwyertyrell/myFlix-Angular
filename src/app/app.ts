b43b0986413108ccf3d608f79fddcf6783421042
dwyertyrell <Dwyertyrell@gmail.com>
2025-08-23 14:07:18 +0100

    Refactor app entry and add user registration dialog
    Replaces the placeholder app.html with a custom welcome page and signup/login buttons. Integrates Angular Material modules and dialog for user registration, moves HTTP client provider to app.config, and updates main.ts to use appConfig for bootstrapping.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,16 +1,56 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
-
+import { NgStyle } from '@angular/common';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDialogModule, MatDialog } from '@angular/material/dialog';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { 
+  FormGroup, 
+  FormControl, 
+  FormsModule, 
+  NgModel, 
+  ReactiveFormsModule, 
+} from '@angular/forms';
+import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
+  imports: [
+    FormsModule, 
+    ReactiveFormsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatDialogModule,
+    MatSnackBarModule
+  ],
+  templateUrl: './app.html', 
   styleUrl: './app.scss'
 })
 
-
 export class App {
-  protected readonly title = signal('myFlix-Angular-client');
+ title = 'myFlix-Angular-client';
+
+  constructor(public dialog: MatDialog) { }
+// This is the function that will open the dialog when the signup button is clicked  
+openUserRegistrationDialog(): void {
+    this.dialog.open(UserRegistrationFormComponent, {
+// Assigning the dialog a width
+    width: '280px'
+    });
+  }
+
 }
+
+
+/*  template: `
+  <h2>hellloooo</h2>
+<p>hi</p>
+
+<input [formControl]="nameControl">
+<p>name:{{nameControl.value}}</p>
+  `, */
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { provideHttpClient } from '@angular/common/http';
+import { appConfig } from './app/app.config';
 import { App } from './app/app';
 
-bootstrapApplication(App, {
-  providers: [
-    provideHttpClient()
-  ]
-})
+bootstrapApplication(App, appConfig)
   .catch((err: unknown) => console.error(err));
