404b093652874c4f6eeb3a5c738111f524d42eaf
dwyertyrell <Dwyertyrell@gmail.com>
2025-08-23 14:06:56 +0100

    Add user registration form component
    Introduces UserRegistrationFormComponent with HTML template, SCSS, TypeScript logic, and basic unit test. The component provides a user registration form with fields for username, password, email, and birthday, and handles registration via the UserRegistrationService.

---
+++ src/app/user-registration-form/user-registration-form.ts
@@ -0,0 +1,63 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+// You'll use this import to close the dialog on success
+import { MatDialogRef } from '@angular/material/dialog';
+
+// This import brings in the API calls created in the fetch-api file
+import { UserRegistrationService } from '../app/../fetch-api-data';
+
+// This import is used to display notifications back to the user
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDialogModule, MatDialog } from '@angular/material/dialog';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { FormsModule } from '@angular/forms';
+
+
+
+@Component({
+  selector: 'app-user-registration-form',
+  templateUrl: './user-registration-form.html',
+  styleUrls: ['./user-registration-form.scss'],
+  imports: [
+    FormsModule, 
+    MatInputModule,
+    MatButtonModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatDialogModule,
+    MatSnackBarModule,
+  ] 
+})
+export class UserRegistrationFormComponent implements OnInit {
+
+  @Input() userData = { Username: '', Password: '', Email: '', Birthday: '' };
+
+constructor(
+    public fetchApiData: UserRegistrationService,
+    public dialogRef: MatDialogRef<UserRegistrationFormComponent>,
+    public snackBar: MatSnackBar) { }
+
+ngOnInit(): void {
+}
+
+// This is the function responsible for sending the form inputs to the backend
+registerUser(): void {
+    this.fetchApiData.userRegistration(this.userData).subscribe((result) => {
+  // Logic for a successful user registration goes here! (To be implemented)
+     this.dialogRef.close(); // This will close the modal on success!
+     this.snackBar.open(result, 'OK', {
+        duration: 2000
+     });
+    }, (result) => {
+      console.log('sign up button is working')
+      this.snackBar.open(result, 'OK', {
+        duration: 2000
+      });
+    });
+  }
+
+  }
